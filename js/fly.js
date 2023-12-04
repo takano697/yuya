53cb6b542f9af3f977d7dc3f9ac64429bc48278b
takano697 <106021983+takano697@users.noreply.github.com>
2023-12-05 08:54:26 +0900

    Update fly.js

--- js/fly.js
+++ js/fly.js
@@ -69,9 +69,9 @@
  	const geometry = new THREE.SphereGeometry( 8.0, 5, 5 );
 	const material = new THREE.MeshPhongMaterial({color: 0xE83845});
 	const sphere = new THREE.Mesh(geometry, material);
-	sphere.position.y = 280;
-	sphere.position.x = -250;
-	sphere.position.z = -230;
+	sphere.position.y = 300;
+	sphere.position.x = -300;
+	sphere.position.z = -280;
 	    
 	    sphere.name ='sphere';
 	    
