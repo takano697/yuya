062fe2bf1ae1e9887a7bf3869350398f3f622bcb
takano697 <106021983+takano697@users.noreply.github.com>
2023-12-05 09:44:09 +0900

    Update fly.js

--- js/fly.js
+++ js/fly.js
@@ -248,7 +248,7 @@
 				count = 0;
                             doorFlg = false;
                         }
-			let lightcount,lightcount2 = 0;
+			let lightcount = 0,lightcount2 = 0;
 			if(lightFlg && lightcount == 0){
 			for(i=0;i > light1.length; i++){
 			light1[i].material.emissive.setRGB(1,1,1);
